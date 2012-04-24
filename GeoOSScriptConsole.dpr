ce62730035835123d55e34fd5dbfb2766ecf3ce5
David Klepacek <david.klepacek@seznam.cz>
2012-04-24 09:06:31 +0200

    First Upload

---
+++ GeoOSScriptConsole.dpr
@@ -0,0 +1,15 @@
+program GeoOSScriptConsole;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  SysUtils,
+  SevenZipVCL in 'SevenZipVCL.pas';
+
+begin
+  writeln('Starting...');
+  writeln('Completed!');
+  readln;
+end.
