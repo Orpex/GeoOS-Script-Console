754faf00651127984dfa03c7db85f038f3a61578
David Klepáček <david.klepacek@seznam.cz>
2012-04-25 18:47:09 +0300

    Added several new functions, including registry acces, but not fully initialized

--- GeoOSScriptConsole.dpr
+++ GeoOSScriptConsole.dpr
@@ -6,11 +6,13 @@ program GeoOSScriptConsole;
 
 uses
   SysUtils,  //Only for Windows
-  Classes; //Contains several Useful things
+  Classes, //Contains several Useful things
+  Registry; //implement Windows registry
 
 var
-  paramsraw: string;        // implement variables for recognition
-  params: TStringList;      // of program parameters (max up to 50 params)
+  paramsraw: string;        // implement variables for recognition of
+  params: TStringList;      // program parameters (max up to 50 params)
+  reg: TRegistry;           // variable for accessing Windows registry
 
 procedure Split(Delimiter: Char; Str: string; ListOfStrings: TStrings); // Split what we need
 //thanks to RRUZ - http://stackoverflow.com/questions/2625707/delphi-how-do-i-split-a-string-into-an-array-of-strings-based-on-a-delimiter
@@ -37,14 +39,17 @@ for i:=1 to ParamCount() do
   end;
 end;
 
-function GetParams(): string; //gets all params
-//needs to be proper implemented
+function GetParams(): string; //gets all parameters
 var
   returnstr: string;
+  i: integer;
 begin
 returnstr:='';
-for i:=1 to  do
-
+for i:=1 to ParamCount() do
+begin
+  returnstr:=returnstr+ParamStr(i)+'|';
+end;
+result:=returnstr;
 end;
 
 function LookUpForParams(): string; //Search, how many and what parameters are used
@@ -58,9 +63,9 @@ end;
 function SearchForSplitParam(param: string): boolean;
 var index: integer;
 begin
-  index:=-1;  //because index cannot be negative numbers
+  index:=-1;  //because index cannot be negative
   params.Find(param,index);
-  if not(index=-1) then //if index of given searched string isn't find, value of 'index' isn't changed
+  if not(index=-1) then //if index of given searched string isn't found, value of 'index' is still -1
   begin
     result:=true; //param is found
   end
@@ -72,8 +77,13 @@ end;
 
 begin
   writeln('Starting...'); // Starting of script
-  paramsraw:=LookUpForParams(); //Main initializon for parameters... what to do and everything els
+  paramsraw:=LookUpForParams(); //Main initializon for parameters... what to do and everything else
   Split('|',paramsraw,params); //Get every param used
+  // initialize registry variable
+  reg:=TRegistry.Create();
+  reg.RootKey:=HKEY_CURRENT_USER;
+  if(reg.KeyExists())
+  reg.OpenKey()
   // Now we need if it would be an install (or update) or uninstall (remove or downgrade)
   if(SearchForSplitParam('-i') and not(SearchForSplitParam('-r'))) then
   begin
@@ -92,6 +102,7 @@ begin
     writeln('Parameters are incorrect! Found both -i and -r!');
     exit; //terminate program
   end;
+  reg.Free; //release memory from using registry variable
   writeln('Completed!'); // THE END
   readln;
 end.
