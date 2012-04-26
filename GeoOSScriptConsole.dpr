29f93accba0e7b3db52ed8975f7fa88fc410337a
David Klepáček <david.klepacek@seznam.cz>
2012-04-26 21:26:44 +0300

    Registry implemented, added some new functions

--- GeoOSScriptConsole.dpr
+++ GeoOSScriptConsole.dpr
@@ -5,9 +5,10 @@ program GeoOSScriptConsole;
 {$R *.res}
 
 uses
-  SysUtils,  //Only for Windows
-  Classes, //Contains several Useful things
-  Registry; //implement Windows registry
+  SysUtils, //basic utils
+  Classes,  //some useful classes
+  Registry, //implement Windows registry
+  Windows;  //declaration and etc., useful for us
 
 var
   paramsraw: string;        // implement variables for recognition of
@@ -20,23 +21,6 @@ begin
    ListOfStrings.Clear;
    ListOfStrings.Delimiter     := Delimiter;
    ListOfStrings.DelimitedText := Str;
-end;
-
-function SearchForParam(param: string): boolean; //If searched parameter is in command line
-//not needed right now
-var i: integer;
-begin
-for i:=1 to ParamCount() do
-  begin
-    if(ParamStr(i)=param) then
-    begin
-      result:=true;
-    end
-    else
-    begin
-      result:=false;
-    end;
-  end;
 end;
 
 function GetParams(): string; //gets all parameters
@@ -75,6 +59,22 @@ begin
   end;
 end;
 
+function GetInitIndex(param: char): integer; //gets index of -i or -i parameters (of ParamStrs)
+var index: integer;
+begin
+  index:=-1;  //because index cannot be negative
+  params.Find('-'+param,index);
+  //we know, that it already exists, so there is no condition for: if index is not -1
+  result:=index;
+end;
+
+function IsRemote(param: string): boolean; //Local -> false | Remote -> true
+begin
+  if(param[1]+param[2]+param[3]+param[4]='http') then result:=true //accepting http:// and https:// as remotes
+  else if(param[1]+param[2]+param[3]+param[4]='ftp') then result:=true //accepting ftp as remote
+  else result:=false; //everything else is in local computer
+end;
+
 begin
   writeln('Starting...'); // Starting of script
   paramsraw:=LookUpForParams(); //Main initializon for parameters... what to do and everything else
@@ -82,14 +82,35 @@ begin
   // initialize registry variable
   reg:=TRegistry.Create();
   reg.RootKey:=HKEY_CURRENT_USER;
-  if(reg.KeyExists())
-  reg.OpenKey()
+  if(reg.KeyExists('Software\GeoOS-Script\')) then
+  begin
+    reg.OpenKey('Software\GeoOS-Script\',false);
+  end
+  else
+  begin
+    reg.CreateKey('Software\GeoOS-Script\');
+    reg.OpenKey('Software\GeoOS-Script\',false);
+  end;
   // Now we need if it would be an install (or update) or uninstall (remove or downgrade)
   if(SearchForSplitParam('-i') and not(SearchForSplitParam('-r'))) then
   begin
     //Install script or update (-i means install)
     //If -r (-r means remove) is found too, params are incorrect
+    if(IsRemote(ParamStr(GetInitIndex('i')))) then
+    begin
 
+    end
+    else // parameter after -i is local? check it
+    begin
+      {if(FileExists()) then
+      begin
+        //file exists
+      end
+      else
+      begin
+        //local file not found, parameter for file is incorrect
+      end;}
+    end;
   end
   else if(SearchForSplitParam('-r') and not(SearchForSplitParam('-i'))) then
   begin
